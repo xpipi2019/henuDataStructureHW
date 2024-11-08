4fc8d74222b90cfa8102962c2f05257252e20a7a
xpipi2019 <1193820217@qq.com>
2024-11-08 22:17:09 +0800

    HW7 二叉树的应用

---
+++ myBiTree.h
@@ -0,0 +1,13 @@
+//
+// Created by XPIPI on 24-11-8.
+//
+
+#ifndef MYBITREE_H
+#define MYBITREE_H
+
+typedef struct BiTNode {
+    char data;
+    struct BiTNode* lchild, *rchild;
+} *BiTree;
+
+#endif //MYBITREE_H
